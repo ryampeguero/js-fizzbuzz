c6b26473b47a6d6971b7339577022a35fe48774e
ryampeguero <ryampeguero@gmail.com>
2024-03-28 09:32:11 +0100

    feat: added text fizz, buzz or fizzbuzz in HTML

--- js/script.js
+++ js/script.js
@@ -12,24 +12,28 @@ for (let i = 0; i <= 100; i++) {
     if(i % 3 === 0 && i % 5 === 0 ){
         //FIZZBUZZ
         boxType = "fizzbuzz"
+        boxElem.innerHTML = boxType;
         console.log("Questo è: fizzbuzz ", i);
     }else if(i % 3 === 0) {
         //fizz
         boxType = "fizz"
+        boxElem.innerHTML = boxType;
         console.log("Questo è: fizz ", i);
     }else if(i % 5 === 0) {
         //BUZZ
         boxType = "buzz"
+        boxElem.innerHTML = boxType;
         console.log("Questo è: buzz ", i);
     }else {
         boxType = "normal"
+        boxElem.innerHTML = i;
         console.log("Normale: ", i);
     }
 
     //OUTPUT
     boxElem.classList.add("box");
     boxElem.classList.add(boxType);
-    boxElem.innerHTML = i;
+    
     console.log("Eccomi");
     rowElem.append(boxElem);
 }
